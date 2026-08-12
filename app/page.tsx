980e1d3b5f220d0f08c8f25b83ef1dce4b2cc5c8
Dimonchik2609 <magnit2006@gmail.com>
2026-08-12 05:18:42 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import css from './Home.module.css';
+import css from './page.module.css';
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
